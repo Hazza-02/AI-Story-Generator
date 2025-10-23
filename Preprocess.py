66add4b9ffa465d2ccfe8a6ac8186626305d3fba
Harry Chambers <harrychambers2002@gmail.com>
2025-10-23 14:28:25 +0100

    Overview of the class

--- Preprocess.py
+++ Preprocess.py
@@ -1,1 +1,12 @@
 import os
+
+"""
+The preprocess class will save a copy of the target files in the writingPrompts dataset
+These target files will be what the model learns of 
+On the Kaggle page for the dataset they recommend truncating the stories to 1000 words so that will be implemented here and expermented with
+"""
+
+class Preprocess:
+
+    def __init__(self, dataset_path):
+        self.dataset_path = dataset_path
