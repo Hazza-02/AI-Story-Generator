63db1270c5abf5d7cf266763e89e8fe6b6a8e217
Harry Chambers <harrychambers2002@gmail.com>
2025-10-23 18:54:12 +0100

    Truncate method for preprocessor

--- Preprocess.py
+++ Preprocess.py
@@ -8,5 +8,36 @@ On the Kaggle page for the dataset they recommend truncating the stories to 1000
 
 class Preprocess:
 
-    def __init__(self, dataset_path):
+    def __init__(self, dataset_path, max_story_length=1000, save_name="_processed"):
         self.dataset_path = dataset_path
+        self.max_story_length = max_story_length
+        self.save_name = save_name
+
+    # Truncate stories to max_story_length and save to new files
+    def truncate_stories(self):
+        data = ['test', 'train', 'valid']
+        for name in data:
+
+            input_path = os.path.join(self.dataset_path, f"{name}.wp_target")
+            output_path = os.path.join(self.dataset_path, f"{name}{self.save_name}.wp_target")
+
+            # Check if input file exists
+            if not os.path.exists(input_path):
+              print(f"File {input_path} does not exist. Skipping!")
+              continue
+            
+            # Open file and read stories (There is one story per line)
+            with open(input_path) as f:
+                stories = f.readlines()
+
+            truncated_stories = [" ".join(story.split()[:self.max_story_length]) for story in stories]
+
+            with open(output_path, "w") as f:
+              for story in truncated_stories:
+                f.write(story.strip() + "\n")
+
+if __name__ == "__main__":
+    
+    dataset_path = "./writingPrompts"
+    preprocessor = Preprocess(dataset_path)
+    preprocessor.truncate_stories()
